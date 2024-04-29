48efcf3b876c38072a5df114844a70e1e48753b4
EnricoVBerto <enricovberto@hotmail.com>
2024-04-29 17:35:06 -0300

    Mostrar mapa

--- teste.py
+++ teste.py
@@ -65,3 +65,15 @@ def criar_estoque_computador():
     pais_computador = random.choice(paises_list)
     estoque_computador = PAISES[pais_computador]
     return estoque_computador, pais_computador
+def mostrar_mapa(mapa, mapa_inimigo):
+    print("  A B C D E F G H I J")
+    for i, linha in enumerate(mapa, start=1):
+        print(f"{i:2}", end=" ")
+        for j, elemento in enumerate(linha):
+            if mapa_inimigo[i-1][j] == ' ':
+                print(mapa_inimigo[i-1][j], end=" ")
+            elif mapa_inimigo[i-1][j] == 'X':
+                print('X', end=" ")
+            else:
+                print('□', end=" ")
+        print()
